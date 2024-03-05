8b9178183829711a956d3199fdd98a0589fd2687
ip-repo <123945379+ip-repo@users.noreply.github.com>
2024-03-05 06:18:25 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+var synth = window.speechSynthesis;
+var voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    var select = document.getElementById("voiceSelect");
+    for(var i = 0; i < voices.length ; i++) {
+      var option = document.createElement("option");
+      option.textContent = voices[i].name + " (" + voices[i].lang + ")";
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      select.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+  function speak(){
+    var textInput = document.getElementById("text-to-speech").value;
+    var voiceSelect = document.getElementById("voiceSelect");
+    var utterThis = new SpeechSynthesisUtterance(textInput);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for(var i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+    synth.speak(utterThis);
+  }
+
+  function stopSpeaking(){
+    synth.cancel();
+  }
